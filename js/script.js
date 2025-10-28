f3cb65426bc8c275b93fd2d59e4336696471d5e6
DavidZarfati <zarda2001@gmail.com>
2025-10-28 15:15:20 +0100

    feat:commento esplicativo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // console.log("hello world");
+
+//Progetto
+// Stampa i numeri da 1 a 100
+// se il numero è multiplo di 3,stampo Fizz (%3 con resto 0)
+// se il numero è multiplo di 5,stampo Buzz (%5 con resto 0)
+// se è multiplo di entrambi stampo FizzBuzz
+
+
+
+
+
+// informazioni
+
+
+
+
+//operazioni
+
+
+
+
+
+//output
