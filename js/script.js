06d40e49850c2a2913cf3a6a7439277b76634256
DavidZarfati <zarda2001@gmail.com>
2025-10-28 15:27:56 +0100

    feat:Secondo modo di risolvere il programma

--- js/script.js
+++ js/script.js
@@ -8,17 +8,33 @@
 
 
 //operazioni
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuzz")
+//     }
+//     else if (i % 3 == 0) {
+//         console.log("Fizz")
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("Buzz")
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
+let risultato = 0
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+        risultato = "FizzBuzz"
     }
     else if (i % 3 == 0) {
-        console.log("Fizz")
+        risultato = "Fizz"
     }
     else if (i % 5 == 0) {
-        console.log("Buzz")
+        risultato = "Buzz"
     }
     else {
-        console.log(i)
+        risultato = i
     }
+    console.log(risultato)
 }
